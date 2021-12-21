94519fb00d2334cbdb0565544bd6f4f4afccc0ec
Caio-Dias02 <you@example.com>
2021-12-21 14:27:03 -0300

    Project Finished

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -21,7 +21,7 @@ require_once 'tarefa_controller.php';
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
 	<script>
-		function editar(id, txt_tarefa){
+		function editar(id, txt_tarefa) {
 			//criar um form de edição
 			let form = document.createElement('form')
 			form.action = 'tarefa_controller.php?acao=atualizar'
@@ -62,14 +62,21 @@ require_once 'tarefa_controller.php';
 			console.log(form)
 
 			//selecionar a div tarefa
-			let tarefa = document.getElementById('tarefa_'+id)
+			let tarefa = document.getElementById('tarefa_' + id)
 
 			//limpar o texto da tarefa para inclusão do form
 			tarefa.innerHTML = ''
 
 			//incluir form na página
 			tarefa.insertBefore(form, tarefa[0])
+		}
 
+		function remover(id) {
+			location.href = "todas_tarefas.php?acao=remover&id=" + id
+		}
+
+		function marcarRealizada(id) {
+			location.href = "todas_tarefas.php?acao=marcarRealizada&id=" + id
 		}
 	</script>
 
@@ -102,19 +109,23 @@ require_once 'tarefa_controller.php';
 							<h4>Todas tarefas</h4>
 							<hr />
 
-							<?php foreach($tarefas as $indice => $tarefa) { ?>
-									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
-											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
-										</div>
-										<div class="col-sm-3 mt-2 d-flex justify-content-between">
-											<i class="fas fa-trash-alt fa-lg text-danger" style="cursor: pointer;"></i>
-											<i class="fas fa-edit fa-lg text-info" style="cursor: pointer;" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-											<i class="fas fa-check-square fa-lg text-success" style="cursor: pointer;"></i>
-										</div>
+							<?php foreach ($tarefas as $indice => $tarefa) { ?>
+								<div class="row mb-3 d-flex align-items-center tarefa">
+									<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+										<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
 									</div>
+									<div class="col-sm-3 mt-2 d-flex justify-content-between">
+										<i class="fas fa-trash-alt fa-lg text-danger" style="cursor: pointer;" onclick="remover(<?= $tarefa->id ?>)"></i>
 
-								<?php } ?>
+
+										<?php if ($tarefa->status == 'pendente') { ?>
+											<i class="fas fa-edit fa-lg text-info" style="cursor: pointer;" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+											<i class="fas fa-check-square fa-lg text-success" style="cursor: pointer;" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+										<?php } ?>
+									</div>
+								</div>
+
+							<?php } ?>
 
 						</div>
 					</div>
